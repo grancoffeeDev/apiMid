8fc4d00ff64369085f29a83f7e1e59f357c03a34
nascimento92 <gabriel_maker@hotmail.com>
2022-11-07 17:59:44 -0300

    a

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
-from fastapi import FastAPI
-from model.teste import gcteste
-from connection.config import Database
-import json
+from flask import Flask
 
-#python -m uvicorn main:app --reload
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route('/', methods=['GET'])
+def get_gc_teste():
+    return "oi"
 
-@app.get("/")
-def home() :
-    return {"teste"}
-
-# Tabela public.gc_teste
-@app.get("/gc_teste")
-def get_teste():
-   query = "select * from public.gc_teste"
-   db = Database()
-   db.connect()
-   r = db.executar(query, '', 'GET')
-   db.close()
-   return json.loads(r)
-
-@app.post("/gc_teste")
-def post_teste(gc_teste: gcteste):
-   query = "INSERT INTO public.gc_teste(nome) VALUES('{0}')".format(gc_teste.nome)
-   db = Database()
-   db.connect()
-   db.executar(query, (gc_teste.nome), 'POST')
-   db.close()
-   return {"resultado":"Dados inseridos.", "nome":gc_teste.nome}
-    
+app.run()
