f96fc5c8e9d4c9816be430f7ee91d678a25f4060
nascimento92 <gabriel_maker@hotmail.com>
2022-11-07 18:28:38 -0300

    aa

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_gc_teste():
-    return "oi"
+    return make_response(jsonify(mensagem='Testando essa API'))
 
-app.run()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
