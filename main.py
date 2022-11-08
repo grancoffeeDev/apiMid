0b3845b9fca0e452b2bb9a04115126d3783b4dcb
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 13:28:02 -0300

    aa

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_gc_teste():
-    return make_response(jsonify(mensagem='Testando essa API'))
+    return make_response(jsonify(mensagem='Testando essa API !!'))
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
