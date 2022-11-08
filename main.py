bea494d6209f99d238525366e41deb06c996a1b1
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 16:35:51 -0300

    a

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import *
 import psycopg2
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_gc_teste():
-    return make_response(jsonify(mensagem='API do MID'))  
+    return make_response(jsonify(mensagem='API do MID'))
+
+# Tabela public.gc_teste
+@app.route('/gc_teste', methods=['GET'])
+def get_teste():
+    conn = psycopg2.connect(
+        host="34.95.183.152",
+        database="TelemetriaGC",
+        port="5432",
+        user="postgres",
+        password="VnBgPQbYzwa95VDm")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM GC_TESTE;')
+    return json.dumps(cur.fetchall(),indent=4) 
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=8080, debug=True)
