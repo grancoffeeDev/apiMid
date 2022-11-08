ce3dc83a38d0791cbb725ab14e60af8cd2d8a695
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 16:28:45 -0300

    a

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def get_gc_teste():
     return make_response(jsonify(mensagem='API do MID'))  
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
     #app.run(host='127.0.0.1', port=8080, debug=True)
-    #app.run()
+    app.run()
