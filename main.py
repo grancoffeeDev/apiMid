5a37359d434221d6649b3babf59f203c64a59df4
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 13:37:53 -0300

    aaa

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import *
+from connection.config import Database
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_gc_teste():
     return make_response(jsonify(mensagem='Testando essa API !!'))
+
+# Tabela public.gc_teste
+@app.route('/gc_teste', methods=['GET'])
+def get_teste():
+   query = "select * from public.gc_teste"
+   db = Database()
+   db.connect()
+   r = db.executar(query, '', 'GET')
+   db.close()
+   return json.loads(r)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
