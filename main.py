f2df5463ff87c0f044048668b5a21751019fa2e9
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 16:01:34 -0300

    aa

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from flask import *
-from connection.config import Database
+import os
+import psycopg2
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_gc_teste():
-    return make_response(jsonify(mensagem='Testando essa API !!'))
+    return make_response(jsonify(mensagem='API do MID'))
 
 # Tabela public.gc_teste
 @app.route('/gc_teste', methods=['GET'])
 def get_teste():
-   query = "select * from public.gc_teste"
-   db = Database()
-   db.connect()
-   r = db.executar(query, '', 'GET')
-   db.close()
-   return json.loads(r)
+    conn = psycopg2.connect(
+        host="34.95.183.152",
+        database="TelemetriaGC",
+        port="5432",
+        user=os.environ['SQL_USER'],
+        password="VnBgPQbYzwa95VDm")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM GC_TESTE;')
+    return json.dumps(cur.fetchall(),indent=4)
+    
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
-    #app.run(host='127.0.0.1', port=8080, debug=True)
-#app.run()
+    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    #app.run()
--- model/teste.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-from typing import Union
-
-class gcteste(BaseModel):
-    nome: str
