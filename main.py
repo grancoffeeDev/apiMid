d3e72bd1d06cfbeda6c7cb83d79bc11463ca96a9
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 16:29:09 -0300

    a

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ def get_gc_teste():
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=8080, debug=True)
-    #app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
+    #app.run()
