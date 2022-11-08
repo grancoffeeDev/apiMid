9269f824aad19167416afaea8775376cea5f46f9
Gabriel da Silva Nascimento <gabriel.nascimento@GCF.local>
2022-11-08 16:50:04 -0300

    aa

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ def get_teste():
     return json.dumps(cur.fetchall(),indent=4) 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
     #app.run()
